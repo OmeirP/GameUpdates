638bdef6201eb0b0beb8168d4bc6516c35bc9604
OmeirP <op6666667@gmail.com>
2026-04-02 21:45:02 +0100

    Using environment variables

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import httpx
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
 
 app = FastAPI()
 
-
-
-# Allow your React dev server to talk to the API
+# allow react server to talk to api
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
