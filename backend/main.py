4d6194c84f0655e961859c46c47490364242ec59
OmeirP <op6666667@gmail.com>
2026-04-03 16:49:08 +0100

    Correct query function for igdb

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 import httpx
 from dotenv import load_dotenv
 import os
+import time
 
 load_dotenv()
 client_id = os.getenv("CLIENT_ID")
@@ -20,7 +21,7 @@ app.add_middleware(
 )
 
 
-# Get token
+# Get token, doing it this way is synchronous
 response = httpx.post("https://id.twitch.tv/oauth2/token", params={
     "client_id" : client_id,
     "client_secret" : client_secret,
@@ -29,9 +30,36 @@ response = httpx.post("https://id.twitch.tv/oauth2/token", params={
 
 token = response.json()["access_token"]
 
+
+
+class IGDBGame(BaseModel):
+    name: str
+    first_release_date: int = None
+
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "From FastAPI inside Docker"}
 
 
+@app.get("/upcoming-releases")
+async def get_upcoming():
     
+    headers = {
+        "Client-ID": client_id,
+        "Authorization": f"Bearer {token}"
+    }
+    
+    query = f"fields name, first_release_date; limit 20; where first_release_date >= {int(time.time())}; sort first_release_date asc;"
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.post("https://api.igdb.com/v4/games", headers=headers, content=query)
+        
+    
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="IGDB Request Failed")
+    
+    data = response.json()
+    print(data)
+    return [IGDBGame.model_validate(game) for game in data]
