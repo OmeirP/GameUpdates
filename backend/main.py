308489654c4c15d72544e89bf58713d8cd8b1f4d
OmeirP <op6666667@gmail.com>
2026-04-03 02:37:34 +0100

    Getting access token

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import httpx
 from dotenv import load_dotenv
 import os
@@ -18,6 +19,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+# Get token
+response = httpx.post("https://id.twitch.tv/oauth2/token", params={
+    "client_id" : client_id,
+    "client_secret" : client_secret,
+    "grant_type" : "client_credentials"
+})
+
+token = response.json()["access_token"]
+
 @app.get("/")
 def read_root():
     return {"Hello": "From FastAPI inside Docker"}
+
+
+    
