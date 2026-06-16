6d62d0c19493bf26d3af0cc2b28fbeb40b70261f
OmeirP <op6666667@gmail.com>
2026-06-16 20:49:54 +0100

    Added 127.0.0.1 to allow list, since thats different from localhost apparently

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 # allow react server to talk to api
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
