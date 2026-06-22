7ecc1a9b7012dfda28c7a98d062d4f59101cfdcb
OmeirP <op6666667@gmail.com>
2026-06-22 20:13:02 +0100

    Corrected http error message and added conditional release date showing on a card.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ const mockGames = [
   { id: 2, name: "Cyberpunk 2077", first_release_date: 1607558400 }
 ];
 
-const GameRow = ({ heading, endpoint }) => {
+const GameRow = ({ heading, endpoint, showReleaseDate = false }) => {
   const [games, setGames] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -17,7 +17,7 @@ const GameRow = ({ heading, endpoint }) => {
         const response = await fetch(`http://localhost:8000${endpoint}`);
 
         if (!response.ok) {
-          throw new Error('HTTP error! Status: ${response.status}');
+          throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
         const data = await response.json();
@@ -41,9 +41,11 @@ const GameRow = ({ heading, endpoint }) => {
       <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-10 gap-8">
         {games.slice(0,10).map(game => (
           <div key={game.id} className="bg-slate-800 rounded-lg border-2 border-gray-800">
-            <img className="rounded-t-lg border-b-2 border-gray-700 w-full aspect-3/4 object-cover bg-slate-900" src={game.cover_url} />
-            <p className="font-bold text-indigo-400 ml-2 mt-1">{new Date(game.first_release_date*1000).toLocaleDateString()}</p>
-            <p className="font-semibold text-white ml-2 mb-2">{game.name}</p>
+            <img className="rounded-t-lg border-b-2 border-gray-700 w-full aspect-3/4 object-cover bg-slate-900 mb-1" src={game.cover_url} />
+            {game.first_release_date && ( 
+              <p className="font-bold text-indigo-400 mx-2">{new Date(game.first_release_date*1000).toLocaleDateString()}</p>
+            )}
+            <p className="font-semibold text-white mx-2 mb-2">{game.name}</p>
             
           </div>
         ))}
@@ -58,7 +60,8 @@ function App() {
   return (
     <main className="flex-1 p-8 bg-slate-950"> 
       <h1 className="text-center mx-auto w-full max-w-7xl font-semibold text-3xl text-white">Dashboard</h1>
-      <GameRow heading="Upcoming" endpoint="/upcoming-releases" />
+      <GameRow heading="Upcoming Releases" endpoint="/upcoming-releases" showReleaseDate={true}/>
+      <GameRow heading="Top Rated" endpoint="/top-rated-year" showReleaseDate={true}/>
     </main>
     
   );
