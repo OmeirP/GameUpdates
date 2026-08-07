5e62d857e543106c3893c59e7df7f50618599969
OmeirP <op6666667@gmail.com>
2026-08-07 01:03:26 +0100

    Using ssl connection when creating engine

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,7 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=True)   # Probably remove echo in prod
+engine = create_async_engine(DATABASE_URL, echo=True, connect_args={"ssl": True})   # Probably remove echo in prod. Neon requires ssl - this should be the correct way to do it for asyncpg.
 
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
