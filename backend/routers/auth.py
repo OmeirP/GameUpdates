8705e0230cc60c20cda837a43f623e2874239ac2
OmeirP <op6666667@gmail.com>
2026-08-17 22:16:20 +0100

    Changed: Bearer string in cookie value, added UserRead to request body of login endpoint, switched to custom schema instead of OAuth2PasswordRequestForm

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,28 +1,27 @@
 from fastapi import APIRouter, Depends, Response, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import select
 from sqlalchemy.exc import IntegrityError
 from auth import hash_password, verify_password, create_access_token
-from models import User, UserCreate, UserRead, SignupResponse
+from models import User, UserCreate, UserRead, LoginRequest, SignupResponse
 from database import AsyncSession, get_session
 
 
 router = APIRouter(
-    prefix="/auth",     # Prepends "/games" to all routes
-    tags=["Authentication"]       # For FASTAPI docs. Groups the endpoints under Games
+    prefix="/auth",
+    tags=["Authentication"]       # For FASTAPI docs. Groups the endpoints under Authentication
 )
 
 @router.post("/login")
 async def login(
     response: Response,  # For modifying reponse metadata like for setting an http-only cookie
-    form_data: OAuth2PasswordRequestForm = Depends(),   # Depends can be empty here because of type inference. Putting OAuth2PasswordRequestForm inside is redundant
+    credentials: LoginRequest,
     session: AsyncSession = Depends(get_session)
 ):
     
-    statement = select(User).where(User.email == form_data.username)    # Apparently OAuth2PasswordRequestForm takes the first field as username even if it's email
+    statement = select(User).where(User.email == credentials.email)
     user = (await session.exec(statement)).one_or_none()    # session.execf returns an iterable database stream. one_or_none() or first() is needed to get an actual instance. 
     
-    if not user or not verify_password(form_data.password, user.hashed_password):
+    if not user or not verify_password(credentials.password, user.hashed_password):
         # HTTPExceptions just tells fastapi to stop running code and send a HTTP response back over with a specific status code and body
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -31,20 +30,23 @@ async def login(
         )
     
     
-    access_token = create_access_token(data={"sub": str(user.id)})      # Id is required by convention. Maybe add the username?
+    access_token = create_access_token(data={"sub": str(user.id)}) 
     
     
     # HTTP-only cookie currently best practice since supply chain attacks target supposedly trusted js assets
     response.set_cookie(
         key="access_token",
-        value=f"Bearer {access_token}",
+        value=access_token,
         httponly=True,  # Blocks js access
         secure=True,    # Requires HTTPS in production. Most browsers see localhost as special secure contexts so they allow secure cookies over http
         samesite="lax", # Helps mitigate csrf attacks
         max_age=60*60*24*7  # a week
     )
     
-    return {"message": "Logged in successfully"}  # maybe should also contain basic user metadata - username, etc
+    return {"message": "Logged in successfully",        # maybe should also contain basic user metadata - username, etc
+            "user": UserRead.model_validate(user)}  
+
+
 
 
 @router.post("/signup", response_model=SignupResponse, status_code=status.HTTP_201_CREATED)
@@ -81,7 +83,7 @@ async def signup(
     
     response.set_cookie(
     key="access_token",
-    value=f"Bearer {access_token}",
+    value=access_token,
     httponly=True,  # Blocks js access
     secure=True,    # Requires HTTPS in production. Most browsers see localhost as special secure contexts so they allow secure cookies over http
     samesite="lax", # Helps mitigate csrf attacks
