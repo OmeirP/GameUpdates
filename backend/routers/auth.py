f9d0f523c65c03304089ffc0bf59f20160969f87
OmeirP <op6666667@gmail.com>
2026-08-17 14:29:35 +0100

    Wrote signup endpoint

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, Response, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import select
+from sqlalchemy.exc import IntegrityError
 from auth import hash_password, verify_password, create_access_token, Token
-from models import User
+from models import User, UserCreate, SignupResponse
 from database import AsyncSession, get_session
 
 
@@ -46,6 +47,49 @@ async def login(
     return {"message": "Logged in successfully"}  # maybe should also contain basic user metadata - username, etc
 
 
-@router.post("/signup")
-async def signup():
-    pass
+@router.post("/signup", response_model=SignupResponse, status_code=status.HTTP_201_CREATED)
+async def signup(
+    user_in: UserCreate,
+    response: Response,
+    session: AsyncSession
+):
+    db_user = User(
+        email=user_in.email,
+        username=user_in.username,
+        hashed_password=hash_password(user_in.password)
+    )
+    
+    
+    session.add(db_user)
+    
+    try:
+        # Pushes to the db and marks the commited objects as stale
+        await session.commit() 
+        
+        # Basically refreshes/reselects the data from the db so the db_user metadata is no longer marked as stale 
+        await session.refresh(db_user)  # also, the generated primary key is assigned back to the attribute db_user.id. id goes from None to some number
+        
+    except IntegrityError:
+        await session.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="A user with this email or username already exists."
+        )
+        
+        
+    access_token = create_access_token(data={"sub": str(user.id)})
+    
+    response.set_cookie(
+    key="access_token",
+    value=f"Bearer {access_token}",
+    httponly=True,  # Blocks js access
+    secure=True,    # Requires HTTPS in production. Most browsers see localhost as special secure contexts so they allow secure cookies over http
+    samesite="lax", # Helps mitigate csrf attacks
+    max_age=60*60*24*7  # a week
+    )
+    
+    
+    return SignupResponse(
+        message="Account created successfully",
+        user=db_user    # user is of type UserRead, the id is also in the final json output
+    )
